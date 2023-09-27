379bff4da00b2d0782e2da9184e49a19c54c79a6
201496441 <magorothabo1@gmail.com>
2023-09-27 01:42:37 +0000

    Task1

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,10 +1,10 @@
 export default function taskBlock(trueOrFalse) {
-  let task = false;
-  let task2 = true;
+  let task = false; // Use let instead of var
+  let task2 = true; // Use let instead of var
 
   if (trueOrFalse) {
-    task = true;
-    task2 = false;
+    task = true;     // No need to redeclare with let or var inside the block
+    task2 = false;   // No need to redeclare with let or var inside the block
   }
 
   return [task, task2];
